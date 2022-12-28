194c969d35f194d5951f8580cd5ca1d49c259926
reku-chan <113875176+rekuneko@users.noreply.github.com>
2022-12-28 15:03:40 +0800

    Initial commit

---
+++ spring-cashloan/src/main/java/com/springcashloan/springcashloan/SpringCashloanApplication.java
@@ -0,0 +1,13 @@
+package com.springcashloan.springcashloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCashloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCashloanApplication.class, args);
+	}
+
+}
---
+++ spring-cashloan/src/test/java/com/springcashloan/springcashloan/SpringCashloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcashloan.springcashloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCashloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
